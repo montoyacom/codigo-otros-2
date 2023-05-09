8366686a86d7c4b8be1a76e11256e55a9b923bf1
montoya <ing.montoyacom@gmail.com>
2023-05-08 21:16:26 -0600

    Avance sin errores

--- formulario.js
+++ formulario.js
@@ -1,97 +1,74 @@
-var formulario = document.querySelector("#form")
+/*El codigo primero evalua los campos del formulario para verificar que esten
+escritos y aplica algunas validaciones si no se cumplen marca en rojo los input
+luego si todo esta correcto crea un card con los datos del invitado y 
+limpia los campos si es que se crea el card, el boton eliminar borra el nodo
+padre del que se encuentra */
 
-formulario.onsubmit = function(e) {
+let formulario = document.querySelector(".formulario"); // se cambio a .formluario para que agarrara el formulario
+//se cambiaron todos los var por let
+formulario.onsubmit = function (event) {
+  event.preventDefault(); //se le agrefo default
+  //se cambiaroon las variables por unas mas legibles
+  let inputNombre = formulario.elements.names;
+  let inputEdad = formulario.elements.age;
+  let selectNacionalidad = formulario.elements.nationality;
 
-  e.prevent();
-  
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  let nombre = inputNombre.value;
+  let edad = parseInt(inputEdad.value);
+  let nacionalidad = selectNacionalidad.value;
 
-  var nombre = n.value
-  var edad = e.value
+  // se cambiaron los if a poeradores ternarios
+  nombre.trim().length === 0
+    ? inputNombre.classList.add("error")
+    : inputNombre.classList.remove("error");
+  //se agrego un rango de edad realista
+  isNaN(edad) || edad < 18 || edad > 90
+    ? inputEdad.classList.add("error")
+    : inputEdad.classList.remove("error");
+  //se cambio el alert para que tmb tenga la clase error
+  nacionalidad === ""
+    ? selectNacionalidad.classList.add("error")
+    : selectNacionalidad.classList.remove("error");
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
-
-  if (nombre.length === 0) {
-    n.classList.add("error")
+  // se compara que no tenga clase los inputs para poder agregar el card
+  if (
+    inputNombre.classList.value === "" &&
+    inputEdad.classList.value === "" &&
+    selectNacionalidad.classList.value === ""
+  ) {
+    agregarInvitado(nombre, edad, nacionalidad);
+    names.value = "";
+    age.value = "";
+    nationality.value = "";
   }
-  if (edad < 18 || edad > 120) {
-    e.classList.add("error")
-  }
-
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
-  }
-}
-
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
+  //se agrego que se limpien los campos al agregar la card
+};
 
 function agregarInvitado(nombre, edad, nacionalidad) {
+  let lista = document.getElementById("lista-de-invitados");
 
-  if (nacionalidad === "ar") {
-    nacionalidad = "Argentina"
+  let elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista");
+  lista.appendChild(elementoLista);
+
+  function crearElemento(descripcion, valor) {
+    let span = document.createElement("span");
+    span.textContent = descripcion + ": " + valor;
+    //se corrigo todos que fueran inputs los valores que se agregaban al card
+    elementoLista.appendChild(span);
+    elementoLista.appendChild(document.createElement("br"));
   }
-  else if (nacionalidad === "mx") {
-    nacionalidad = "Mexicana"
-  }
-  else if (nacionalidad === "vnzl") {
-    nacionalidad = "Venezolana"
-  }
-  else if (nacionalidad === "per") {
-    nacionalidad = "Peruana"
-  }
 
-var lista = document.getElementById("lista-de-invitados")
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
 
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
+  let botonBorrar = document.createElement("button");
+  botonBorrar.textContent = "Eliminar invitado";
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-}
-
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
-
-
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
-elementoLista.appendChild(botonBorrar);
-
- botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
-  }
+  elementoLista.appendChild(botonBorrar);
+  //se cambio todo el codigo para que el boton solo borre al nodo padre de boton borrar
+  botonBorrar.onclick = function () {
+    elementoLista.remove();
+  };
 }
